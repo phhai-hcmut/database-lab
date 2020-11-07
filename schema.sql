1b403e2341c6b3c16534e33887069bf8c73313b5
Pham Hoang Hai <hai.pham.hoang@hcmut.edu.vn>
2020-11-07 09:16:50 +0700

    Revised schema

--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE track(
 	album_id NOT NULL REFERENCES album(id) ON DELETE CASCADE,
 	track_number INT NOT NULL CHECK (track_number >= 0),
 	name TEXT NOT NULL,
-	length INT NOT NULL CHECK (length > 0),
+	duration INT NOT NULL CHECK (duration > 0),
 	PRIMARY KEY (album_id, track_number)
 );
 CREATE TABLE credit(
@@ -34,7 +34,7 @@ CREATE TABLE playlist(
 	name TEXT NOT NULL,
 	decription TEXT,
 	time_created INT NOT NULL,
-	public INT NOT NULL,
+	is_public INT NOT NULL,
 	PRIMARY KEY (user_id, name)
 );
 CREATE TABLE playlist_content(
@@ -42,10 +42,9 @@ CREATE TABLE playlist_content(
 	name TEXT NOT NULL,
 	album_id INT NOT NULL,
 	track_number INT NOT NULL,
-	track_index INT NOT NULL CHECK (track_index >= 0),
 	time_added INT NOT NULL,
 	FOREIGN KEY (user_id, name) REFERENCES playlist(user_id, name)
-	ON DELETE CASCADE,
+	ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (album_id, track_number) REFERENCES track(album_id, track_number)
 	ON DELETE CASCADE,
 	PRIMARY KEY (user_id, name, album_id, track_number)
@@ -54,17 +53,17 @@ CREATE TABLE user(
 	id INTEGER NOT NULL PRIMARY KEY,
 	online INT NOT NULL,
 	display_name TEXT NOT NULL UNIQUE,
-	repeat INT,
-	playback_status INT,
-	cur_track_idx INT CHECK (cur_track_idx >= 0),
+	repeat_state INT,
+	is_playing INT,
+	cur_queue_idx INT CHECK (cur_queue_idx >= 0),
 	cur_progress INT CHECK (cur_progress >= 0)
 );
-CREATE TABLE queueing(
+CREATE TABLE queue_track(
 	user_id INT NOT NULL REFERENCES user(user_id) ON DELETE CASCADE,
 	album_id INT NOT NULL,
 	track_number INT NOT NULL,
 	queue_index INT NOT NULL CHECK (queue_index >= 0),
 	FOREIGN KEY (album_id, track_number) REFERENCES track(album_id, track_number)
 	ON DELETE CASCADE,
-	PRIMARY KEY (user_id, album_id, track_number)
+	PRIMARY KEY (user_id, queue_index)
 );
