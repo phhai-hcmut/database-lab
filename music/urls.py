da8cc64f45d3826f06c0c353ce837c66025646fc
Long Trần Hoàng <bigtimegangstar@gmail.com>
2020-11-28 16:22:51 +0700

    Added simple View, HTML template, url to get things started

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'music'
 urlpatterns = [
+    # ex: /music/
+    #this one list all albums
     path('', views.index, name='index'),
+    # ex: /music/5/
+    #this one shows one album details
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,12 @@
+from music.models import Album
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    all_album_list = Album.objects.order_by('-name')
+    context = {'all_album_list': all_album_list}
+    return render(request, 'music/index.html', context)
+def detail(request, album_id):
+    return HttpResponse("You're looking at album  %s." % album_id)
