f22107e029dd18e3c98c66a525222353e683fa5d
Long Trần Hoàng <bigtimegangstar@gmail.com>
2020-11-28 16:18:27 +0700

    added functionality to admin Create admin using "python manage.py createsuperuser"

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Track)
+admin.site.register(Credit)
+admin.site.register(User)
+admin.site.register(Playlist)
+admin.site.register(PlaylistContent)
+admin.site.register(QueueTrack)
+admin.site.register(CurrentlyListening)
