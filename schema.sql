4ac7d767085b56fc3ad124f5a717d01f705e93d5
Long Trần Hoàng <bigtimegangstar@gmail.com>
2020-12-09 11:41:33 +0700

    change track's key, rename relations to make sense, added authorization

--- schema.sql
+++ schema.sql
@@ -1,35 +1,36 @@
-CREATE TABLE  IF NOT EXISTS artist(
+CREATE TABLE IF NOT EXISTS artist(
 	id INTEGER NOT NULL PRIMARY KEY,
 	name TEXT NOT NULL
 );
-CREATE TABLE  IF NOT EXISTS album(
+CREATE TABLE IF NOT EXISTS album(
 	id INTEGER NOT NULL PRIMARY KEY,
 	name TEXT NOT NULL,
 	release_date DATE NOT NULL,
 	album_type ALBUM_TYPE NOT NULL
 );
-CREATE TABLE  IF NOT EXISTS ownership(
+CREATE TABLE IF NOT EXISTS ownership(
 	album_id NOT NULL REFERENCES album(id) ON DELETE CASCADE,
 	artist_id NOT NULL REFERENCES artist(id) ON DELETE CASCADE,
 	PRIMARY KEY (album_id, artist_id)
 );
-CREATE TABLE  IF NOT EXISTS track(
-	album_id NOT NULL REFERENCES album(id) ON DELETE CASCADE,
-	track_number INT NOT NULL CHECK (track_number >= 0),
+CREATE TABLE IF NOT EXISTS recording(
+	id INTEGER NOT NULL PRIMARY KEY,
 	name TEXT NOT NULL,
-	duration INT NOT NULL CHECK (duration > 0),
+	duration INT NOT NULL CHECK (duration > 0)
+);
+CREATE TABLE IF NOT EXISTS track(
+	album_id NOT NULL REFERENCES album(id) ON DELETE CASCADE,
+	recording_id NOT NULL REFERENCES recording(id) ON DELETE CASCADE,
+	track_number INT NOT NULL CHECK (track_number >= 0),
 	PRIMARY KEY (album_id, track_number)
 );
-CREATE TABLE  IF NOT EXISTS credit(
-	album_id INT NOT NULL,
-	track_number INT NOT NULL,
+CREATE TABLE IF NOT EXISTS credit(
+	recording_id INT NOT NULL REFERENCES recording(id) ON DELETE CASCADE,
 	artist_id INT NOT NULL REFERENCES artist(id) ON DELETE CASCADE,
 	role CREDIT_ROLE NOT NULL,
-	FOREIGN KEY (album_id, track_number) REFERENCES track(album_id, track_number)
-	ON DELETE CASCADE,
-	PRIMARY KEY (album_id, track_number, artist_id, role)
+	PRIMARY KEY (recording_id, artist_id, role)
 );
-CREATE TABLE  IF NOT EXISTS playlist(
+CREATE TABLE IF NOT EXISTS playlist(
 	user_id INT NOT NULL REFERENCES user(id) ON DELETE CASCADE,
 	name TEXT NOT NULL,
 	description TEXT NOT NULL,
@@ -37,33 +38,27 @@ CREATE TABLE  IF NOT EXISTS playlist(
 	is_public INT NOT NULL,
 	PRIMARY KEY (user_id, name)
 );
-CREATE TABLE  IF NOT EXISTS playlist_content(
+CREATE TABLE IF NOT EXISTS playlist_content(
 	user_id INT NOT NULL,
 	name TEXT NOT NULL,
-	album_id INT NOT NULL,
-	track_number INT NOT NULL,
+	recording_id INT NOT NULL REFERENCES recording(id) ON DELETE CASCADE,
 	time_added TIMESTAMP NOT NULL,
 	FOREIGN KEY (user_id, name) REFERENCES playlist(user_id, name)
 	ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (album_id, track_number) REFERENCES track(album_id, track_number)
-	ON DELETE CASCADE,
-	PRIMARY KEY (user_id, name, album_id, track_number)
+	PRIMARY KEY (user_id, name, recording_id)
 );
-CREATE TABLE  IF NOT EXISTS user(
+CREATE TABLE IF NOT EXISTS user(
 	id INTEGER NOT NULL PRIMARY KEY,
 	online BOOL NOT NULL,
 	display_name TEXT NOT NULL UNIQUE
 );
-CREATE TABLE  IF NOT EXISTS in_queue(
+CREATE TABLE IF NOT EXISTS in_queue(
 	user_id INT NOT NULL REFERENCES user(id) ON DELETE CASCADE,
-	album_id INT NOT NULL,
-	track_number INT NOT NULL,
+	recording_id INT NOT NULL REFERENCES recording(id) ON DELETE CASCADE,
 	queue_index INT NOT NULL CHECK (queue_index >= 0),
-	FOREIGN KEY (album_id, track_number) REFERENCES track(album_id, track_number)
-	ON DELETE CASCADE,
 	PRIMARY KEY (user_id, queue_index)
 );
-CREATE TABLE  IF NOT EXISTS queue(
+CREATE TABLE IF NOT EXISTS user_queue(
 	user_id INT NOT NULL PRIMARY KEY,
 	repeat_state REPEAT_STATE NOT NULL,
 	is_playing BOOL NOT NULL,
@@ -72,3 +67,12 @@ CREATE TABLE  IF NOT EXISTS queue(
 	FOREIGN KEY (user_id, cur_queue_idx) REFERENCES in_queue(user_id, queue_index)
 	ON DELETE CASCADE
 );
+CREATE TABLE if NOT EXISTS authorization (
+	user_id INT NOT NULL REFERENCES user(id) ON DELETE CASCADE,
+  	username text not NULL UNIQUE,
+  	password text not NULL,
+  	role USER_ROLE NOT NULL,
+  	PRIMARY KEY (user_id)
+);
+
+
