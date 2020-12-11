65278057761fad15f3aae9a86d46f1f44c64035e
Long Trần Hoàng <bigtimegangstar@gmail.com>
2020-12-11 21:39:54 +0700

    change schema tables order

--- schema.sql
+++ schema.sql
@@ -2,17 +2,6 @@ CREATE TABLE IF NOT EXISTS artist(
 	artist_id INTEGER NOT NULL PRIMARY KEY,
 	artist_name TEXT NOT NULL
 );
-CREATE TABLE IF NOT EXISTS album(
-	album_id INTEGER NOT NULL PRIMARY KEY,
-	album_name TEXT NOT NULL,
-	release_date DATE NOT NULL,
-	album_type ALBUM_TYPE NOT NULL
-);
-CREATE TABLE IF NOT EXISTS ownership(
-	album_id NOT NULL REFERENCES album(album_id) ON DELETE CASCADE,
-	artist_id NOT NULL REFERENCES artist(artist_id) ON DELETE CASCADE,
-	PRIMARY KEY (album_id, artist_id)
-);
 CREATE TABLE IF NOT EXISTS recording(
 	recording_id INTEGER NOT NULL PRIMARY KEY,
 	recording_name TEXT NOT NULL,
@@ -24,12 +13,33 @@ CREATE TABLE IF NOT EXISTS track(
 	track_number INT NOT NULL CHECK (track_number >= 1),
 	PRIMARY KEY (album_id, track_number)
 );
+CREATE TABLE IF NOT EXISTS album(
+	album_id INTEGER NOT NULL PRIMARY KEY,
+	album_name TEXT NOT NULL,
+	release_date DATE NOT NULL,
+	album_type ALBUM_TYPE NOT NULL
+);
+CREATE TABLE IF NOT EXISTS ownership(
+	album_id NOT NULL REFERENCES album(album_id) ON DELETE CASCADE,
+	artist_id NOT NULL REFERENCES artist(artist_id) ON DELETE CASCADE,
+	PRIMARY KEY (album_id, artist_id)
+);
+
 CREATE TABLE IF NOT EXISTS credit(
 	recording_id INT NOT NULL REFERENCES recording(recording_id) ON DELETE CASCADE,
 	artist_id INT NOT NULL REFERENCES artist(artist_id) ON DELETE CASCADE,
 	role CREDIT_ROLE NOT NULL,
 	PRIMARY KEY (recording_id, artist_id, role)
 );
+
+CREATE TABLE IF NOT EXISTS user(
+	user_id INTEGER NOT NULL PRIMARY KEY,
+	online BOOL NOT NULL,
+	username text not NULL UNIQUE,
+  	password text not NULL,
+  	role USER_ROLE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS playlist(
 	user_id INT NOT NULL REFERENCES user(user_id) ON DELETE CASCADE,
 	playlist_name TEXT NOT NULL,
@@ -46,13 +56,6 @@ CREATE TABLE IF NOT EXISTS playlist_content(
 	FOREIGN KEY (user_id, playlist_name) REFERENCES playlist(user_id, playlist_name)
 	ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY (user_id, playlist_name, recording_id)
-);
-CREATE TABLE IF NOT EXISTS user(
-	user_id INTEGER NOT NULL PRIMARY KEY,
-	online BOOL NOT NULL,
-	username text not NULL UNIQUE,
-  	password text not NULL,
-  	role USER_ROLE NOT NULL
 );
 CREATE TABLE IF NOT EXISTS in_queue(
 	user_id INT NOT NULL REFERENCES user(user_id) ON DELETE CASCADE,
