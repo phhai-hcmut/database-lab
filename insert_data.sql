0996cbea07acac61ce79da219176bd12ba3ac8cb
dirtyg2120 <57407829+dirtyg2120@users.noreply.github.com>
2020-12-11 10:40:43 +0700

    add insert in_queue, user

--- insert_data.sql
+++ insert_data.sql
@@ -1,25 +1,25 @@
 -- recordings --
 
-INSERT into recording(recording_name,duration) VALUES ('Call',295);
-INSERT into recording(recording_name,duration) VALUES ('Sunshine, My Choice',430);
-INSERT into recording(recording_name,duration) VALUES ('Hibernate Magic',341);
-INSERT into recording(recording_name,duration) VALUES ('Cold Era',500);
-INSERT into recording(recording_name,duration) VALUES ('Closer Earth',234);
-INSERT into recording(recording_name,duration) VALUES ('Instrumental Heaven',266);
-INSERT into recording(recording_name,duration) VALUES ('Carebbian Saloon',421);
-INSERT into recording(recording_name,duration) VALUES ('Unused Home',332);
-INSERT into recording(recording_name,duration) VALUES ('The Hottest Soul',267);
-INSERT into recording(recording_name,duration) VALUES ('#That Violin',438);
-INSERT into recording(recording_name,duration) VALUES ('Late Chance',102);
-INSERT into recording(recording_name,duration) VALUES ('The Agent',167);
-INSERT into recording(recording_name,duration) VALUES ('Accepted Life',487);
-INSERT into recording(recording_name,duration) VALUES ('Live Salon',350);
-INSERT into recording(recording_name,duration) VALUES ('Banquet Magic',179);
-INSERT into recording(recording_name,duration) VALUES ('Undiscovered Way',285);
-INSERT into recording(recording_name,duration) VALUES ('Closer Times',137);
-INSERT into recording(recording_name,duration) VALUES ('Not Enough Sleep Loop',113);
-INSERT into recording(recording_name,duration) VALUES ('Light-Hearted Acoustic',252);
-INSERT into recording(recording_name,duration) VALUES ('Torpor Of Lilies',314);
+INSERT INTO recording(recording_name,duration) VALUES ('Call',295);
+INSERT INTO recording(recording_name,duration) VALUES ('Sunshine, My Choice',430);
+INSERT INTO recording(recording_name,duration) VALUES ('Hibernate Magic',341);
+INSERT INTO recording(recording_name,duration) VALUES ('Cold Era',500);
+INSERT INTO recording(recording_name,duration) VALUES ('Closer Earth',234);
+INSERT INTO recording(recording_name,duration) VALUES ('Instrumental Heaven',266);
+INSERT INTO recording(recording_name,duration) VALUES ('Carebbian Saloon',421);
+INSERT INTO recording(recording_name,duration) VALUES ('Unused Home',332);
+INSERT INTO recording(recording_name,duration) VALUES ('The Hottest Soul',267);
+INSERT INTO recording(recording_name,duration) VALUES ('#That Violin',438);
+INSERT INTO recording(recording_name,duration) VALUES ('Late Chance',102);
+INSERT INTO recording(recording_name,duration) VALUES ('The Agent',167);
+INSERT INTO recording(recording_name,duration) VALUES ('Accepted Life',487);
+INSERT INTO recording(recording_name,duration) VALUES ('Live Salon',350);
+INSERT INTO recording(recording_name,duration) VALUES ('Banquet Magic',179);
+INSERT INTO recording(recording_name,duration) VALUES ('Undiscovered Way',285);
+INSERT INTO recording(recording_name,duration) VALUES ('Closer Times',137);
+INSERT INTO recording(recording_name,duration) VALUES ('Not Enough Sleep Loop',113);
+INSERT INTO recording(recording_name,duration) VALUES ('Light-Hearted Acoustic',252);
+INSERT INTO recording(recording_name,duration) VALUES ('Torpor Of Lilies',314);
 
 -- artist --
 
@@ -102,36 +102,61 @@ INSERT INTO ownership(album_id,artist_id) VALUES (10,7);
 
 -- tracks --
 
-INSERT into track(album_id,recording_id,track_number) VALUES (1,8,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (1,9,2);
-INSERT into track(album_id,recording_id,track_number) VALUES (1,10,3);
-INSERT into track(album_id,recording_id,track_number) VALUES (1,4,4);
-INSERT into track(album_id,recording_id,track_number) VALUES (2,5,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (2,5,2);
-INSERT into track(album_id,recording_id,track_number) VALUES (3,8,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (3,6,2);
-INSERT into track(album_id,recording_id,track_number) VALUES (4,7,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (5,4,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (6,5,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (7,6,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (7,7,2);
-INSERT into track(album_id,recording_id,track_number) VALUES (8,8,1);
-INSERT into track(album_id,recording_id,track_number) VALUES (8,5,2);
-INSERT into track(album_id,recording_id,track_number) VALUES (8,10,3);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (1,8,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (1,9,2);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (1,10,3);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (1,4,4);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (2,5,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (2,5,2);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (3,8,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (3,6,2);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (4,7,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (5,4,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (6,5,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (7,6,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (7,7,2);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (8,8,1);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (8,5,2);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (8,10,3);
 -- the albums below have tracks in massed up orders and also duplicated recordings
-INSERT into track(album_id,recording_id,track_number) VALUES (9,5,9);
-INSERT into track(album_id,recording_id,track_number) VALUES (9,5,15);
-INSERT into track(album_id,recording_id,track_number) VALUES (10,3,45);
-INSERT into track(album_id,recording_id,track_number) VALUES (10,3,200);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (9,5,9);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (9,5,15);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (10,3,45);
+INSERT INTO track(album_id,recording_id,track_number) VALUES (10,3,200);
 
 -- user --
-
--- authorization --
-
+INSERT INTO user(online,username,password,role) VALUES (0,'weirdkid','123abc',2);
+INSERT INTO user(online,username,password,role) VALUES (0,'bigtimegangsta','987654sad',2);
+INSERT INTO user(online,username,password,role) VALUES (0,'pphai','haizzzzzzzz',3);
+INSERT INTO user(online,username,password,role) VALUES (0,'someone','abcdefg',0);
+INSERT INTO user(online,username,password,role) VALUES (0,'daddypung','rememberthis',1);
+INSERT INTO user(online,username,password,role) VALUES (0,'whatthehell','mypassword',0);
+INSERT INTO user(online,username,password,role) VALUES (0,'kid_vippro','notvipproanymore',3);
+INSERT INTO user(online,username,password,role) VALUES (0,'pplfuckme','sohard',3);
+INSERT INTO user(online,username,password,role) VALUES (0,'sotired','healthynbalance',0);
+INSERT INTO user(online,username,password,role) VALUES (0,'something','helloworld',1);
 -- user_queue --
 
 -- in_queue --
-
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (1,5,1);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (1,8,2);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (1,15,3);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (2,2,1);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (2,10,2);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (3,11,1);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (3,9,2);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (5,1,1);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (5,3,2);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (5,12,3);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (5,8,4);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (8,11,1);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (8,7,2);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (9,13,1);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (10,16,1);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (10,18,2);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (10,20,3);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (10,1,4);
+INSERT INTO in_queue(user_id,recording_id,queue_index) VALUES (10,2,5);
 -- playlist --
 
 -- playlist_content --
