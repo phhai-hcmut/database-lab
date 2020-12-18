a9ebc4a2fa54608ab268be65daa906da816733cd
Long Trần Hoàng <bigtimegangstar@gmail.com>
2020-12-18 21:15:37 +0700

    Added search 'show more' pages and basically finish search DONE.

--- searching/urls.py
+++ searching/urls.py
@@ -6,4 +6,8 @@ from . import views
 
 urlpatterns = [
     path('search_result/', views.SearchResultView.as_view(), name='search-results'),
+    path('search_result/playlist/<str:query>/',views.searchPlaylist, name='search-playlist'),
+    path('search_result/track/<str:query>/',views.searchTrack, name='search-track'),
+    path('search_result/artist/<str:query>/',views.searchArtist, name='search-artist'),
+    path('search_result/album/<str:query>/',views.searchAlbum, name='search-album'),
 ]
--- searching/views.py
+++ searching/views.py
@@ -1,3 +1,4 @@
+from listening.models import InQueue
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,7 +12,9 @@ from django.db.models import Q
 def searchView(request):
     return render(request, 'structure/navbar.html', None)
 
-
+class QueryHolder:
+    def __init__(self,query_str) -> None:
+        self.value = query_str
 class SearchResultView(ListView):
     template_name = 'searching/search_result.html'
     context_object_name = 'result'
@@ -33,6 +36,43 @@ class SearchResultView(ListView):
         tracks = Track.objects.filter(
             Q(recording__name__icontains=query)
         )
-
-        result = {'albums': albums, 'playlists': playlists, 'artists': artists, 'tracks': tracks}
+        result = {'albums': albums, 'playlists': playlists, 'artists': artists, 'tracks': tracks, 'query' : query}
         return result
+
+
+def searchArtist(request,query):
+    playlist_list = Artist.objects.filter(
+            Q(name__icontains=query)
+        )
+    in_queue = InQueue.objects.filter(user = request.user).order_by('queue_index')
+    # TODO: link html file
+    return render(request, 'searching/list_page/artist_list.html', {'artist_list': playlist_list,
+                                                                  'in_queue': in_queue})
+                                                                
+def searchAlbum(request,query):
+    playlist_list = Album.objects.filter(
+            Q(name__icontains=query)
+        )
+    in_queue = InQueue.objects.filter(user = request.user).order_by('queue_index')
+    # TODO: link html file
+    return render(request, 'searching/list_page/album_list.html', {'album_list': playlist_list,
+                                                                  'in_queue': in_queue})
+def searchTrack(request,query):
+    playlist_list = Track.objects.filter(
+            Q(recording__name__icontains=query)
+        )
+    in_queue = InQueue.objects.filter(user = request.user).order_by('queue_index')
+    # TODO: link html file
+    return render(request, 'searching/list_page/track_list.html', {'track_list': playlist_list,
+                                                                  'in_queue': in_queue})
+def searchPlaylist(request,query):
+    playlist_query = Q(is_public=True)
+    if request.user.is_authenticated:
+        playlist_query = playlist_query | Q(user=request.user)
+    playlist_list = Playlist.objects.filter(
+        Q(name__icontains=query), playlist_query
+    )
+    in_queue = InQueue.objects.filter(user = request.user).order_by('queue_index')
+    # TODO: link html file
+    return render(request, 'searching/list_page/playlist_list.html', {'playlist_list': playlist_list,
+                                                                  'in_queue': in_queue})
