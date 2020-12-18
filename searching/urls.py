bbe8b4a547cad771b32fe7c95ecab9d99c860564
dirtyg2120 <57407829+dirtyg2120@users.noreply.github.com>
2020-12-18 19:26:51 +0700

    Merge searching with every page

--- searching/urls.py
+++ searching/urls.py
@@ -5,6 +5,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.searchView, name='search'),
     path('search_result/', views.SearchResultView.as_view(), name='search-results'),
 ]
--- searching/views.py
+++ searching/views.py
@@ -5,13 +5,18 @@ from music.models import *
 from playlist.models import *
 # ___________________________SEARCH VIEWS__________________________
 from django.views.generic import TemplateView, ListView
-from django.db.models import Q 
+from django.db.models import Q
+
+
 def searchView(request):
-    return render(request, 'searching/search.html',None)
+    return render(request, 'structure/navbar.html', None)
+
+
 class SearchResultView(ListView):
     template_name = 'searching/search_result.html'
     context_object_name = 'result'
-    def get_queryset(self): # new
+
+    def get_queryset(self):  # new
         query = self.request.GET.get('q')
         albums = Album.objects.filter(
             Q(name__icontains=query)
@@ -21,13 +26,13 @@ class SearchResultView(ListView):
             playlist_query = playlist_query | Q(user=self.request.user)
         playlists = Playlist.objects.filter(
             Q(name__icontains=query), playlist_query
-        )   
+        )
         artists = Artist.objects.filter(
             Q(name__icontains=query)
-        )   
+        )
         tracks = Track.objects.filter(
             Q(recording__name__icontains=query)
         )
-        
+
         result = {'albums': albums, 'playlists': playlists, 'artists': artists, 'tracks': tracks}
         return result
