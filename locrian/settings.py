36d17583751e8373fb8a859755661b40fe68234f
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2021-10-30 22:03:12 +0700

    fix: Suppress warning

--- locrian/settings.py
+++ locrian/settings.py
@@ -128,6 +128,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
